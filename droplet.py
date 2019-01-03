187a57314de8df798c6fd5020b553c29010adc4a
taotao720 <44954302+taotao720@users.noreply.github.com>
2019-01-03 13:43:04 -0500

    Update droplet.py

--- droplet.py
+++ droplet.py
@@ -1,18 +1,47 @@
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 
 MIN_SIZE = 500
 
 def Seg(cont,single_area,canvas_size):
     for conti in cont:
         canvas = np.zeros(canvas_size, np.uint8)
-        if cv2.contourArea(conti) > 1.5 * single_area:
+        if cv2.contourArea(conti) > 0.5 * single_area:
             cv2.drawContours(canvas, [conti], 0, 255, cv2.FILLED)
             dist = cv2.distanceTransform(canvas, cv2.DIST_L2, 5)
-            _, centers = cv2.threshold(dist, 0.8 * dist.max(), 255, cv2.THRESH_BINARY)
-            # cv2.imshow('centers',centers)
+            _, cores = cv2.threshold(dist, 0.8 * dist.max(), 255, cv2.THRESH_BINARY)
+            cores = np.uint8(cores)
+            # cv2.imshow('cores',cores)
             # cv2.waitKey(0)
+            _, cores_cont, _ = cv2.findContours(cores, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            for corei in cores_cont:
+                M = cv2.moments(corei)
+                if (M['m00'] == 0):
+                    continue
+                cx = int(M['m10'] / M['m00'])
+                cy = int(M['m01'] / M['m00'])
+
+                cv2.circle(canvas,(cx,cy),2,1,-1)
+
+                rou_list = []
+                for angle in range(1,360):
+                    rou = 2
+                    theta = math.radians(angle)
+                    while (canvas[int(cy + rou * math.cos(theta))][int(cx + rou * math.sin(theta))] != 0):
+                        rou +=1
+                    rou_list.extend([rou])
+
+                fft_val = np.fft.fft(rou_list)
+
+                plt.subplot(211),plt.imshow(canvas)
+                plt.subplot(212),plt.plot(range(1,360),fft_val)
+                plt.show()
+
+
+
+
 
 
 
@@ -31,7 +60,7 @@ img_sharpened = cv2.bitwise_and(img,img_edge)
 
 _,img_binary = cv2.threshold(img_sharpened,120,255,cv2.THRESH_BINARY_INV)
 
-_,cont,hie= cv2.findContours(img_binary,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+_,cont,_= cv2.findContours(img_binary,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
 
 
